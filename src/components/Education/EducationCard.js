07e4aed493ef660a2a8489336f53a545f098dc25
Satyam9993 <satyamkatiyar9993@gmail.com>
2023-03-06 22:58:33 +0530

    new

--- src/components/Education/EducationCard.js
+++ src/components/Education/EducationCard.js
@@ -4,12 +4,15 @@ import EditEducation from "./EditEducation";
 const EducationCard = ({edu}) => {
   return (
     <div className="p-4 lg:w-1/2" style={{minWidth:"15rem"}}>
-      <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+      <div className="h-full w-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+        <div className="rounded-full h-40 w-40">
         <img
           alt="team"
-          className="flex-shrink-0 rounded-lg w-40 h-40 object-cover object-center sm:mb-0 mb-4"
+          // className="flex-shrink-0 rounded-lg w-40 h-40 object-cover object-center sm:mb-0 mb-4"
+          className="h-full w-full"
           src={edu.imageurl}
         />
+        </div>
         <div className="flex-grow sm:pl-8">
           <h2 className="title-font font-medium text-lg text-gray-900">
             {edu.courseName}{" "}
